ca4e1f31477dd4482e9e0755c10880625945574d
Brent Woodruff <brent@fprimex.com>
2020-11-20 18:25:01 -0500

    wip

--- junonia_web.sh
+++ junonia_web.sh
@@ -1,67 +1,155 @@
 #!/bin/sh
 
+junonia_web () {
+  _junonia_cmds="  -curlrc CURLRC=$JUNONIA_CONFIGDIR/curlrc
+  -oauth OAUTH
+  -basic-auth USER:PASSWORD
+$_junonia_cmds
+  "
+
+  readonly JW_INIT=1
+  export JW_INIT
+}
+
 ##
 ## Utility function declarations
 ##
 
-# Replace quotes and newlines with escape characters to prepare the
-# value for insertion into JSON.
-jw_escape_value () {
-  printf '%s' "$1" | awk '
-  {
-    gsub(/"/,"\\\"")
-    gsub(/\\n/,"\\\\n")
-  }
-  NR == 1 {
-    value_line = $0
-  }
-  NR != 1 {
-    value_line = value_line "\\n" $0
-  }
-  END {
-    printf "%s", value_line
-  }'
+# Pretty print JSON as text with each key/value on a single line and no
+# brackets, quotes, or other formatting.
+jw_tree_print () {
+  # $1: JSON body
+  # $2: Optional selector for jq to make the root
+
+  # If a selector was given, select that as the root, otherwise take everything
+  if [ -n "$2" ]; then
+    if ! jw_tree_root="$(printf '%s' "$1" | jq -r "$2")"; then
+      echo "unable to select object(s) using $2"
+    fi
+  else
+    jw_tree_root="$1"
+  fi
+
+  echodebug ""
+  echodebug_raw '%s\n' "$1"
+  if jw_json_tree="$(printf '%s' "$jw_tree_root" | jq -r '
+    def leaf_print(json):
+      json.indent  as $i  |
+      $i + "  "    as $ni |
+      json.element as $e  |
+        (select(($e | type) != "array" and ($e | type) != "object") |
+          "\($e)"),
+        (select(($e | type) == "object" or ($e | type) == "array") |
+          $e | keys[] as $k |
+          "\($k):",
+          "\(leaf_print({"element": $e[$k], "indent": $ni}))");
+
+    leaf_print({"element": ., "indent": ""})')"; then
+    echo "$jw_json_tree"
+  else
+    echo "not a JSON object:"
+    echo "$1"
+    return 1
+  fi
 }
 
+# Perform a curl using the configured authentication and given options.
+# Optionally supply a number and jq selector to retrieve additional pages.
+# Usage:
+#
+# Perform one page request and return the result
+# jq_request <curl url and options>
+#
+# Perform a page request and get pages using the selected url up to a default
+# jq_request <jq selector> <curl url and options>
+#
+# Perform a page request and get pages using the selected url up to a limit
+# jq_request <integer pages to retrieve> <jq selector> <curl url and options>
+#
+# Perform a page request and get next pages using a callback
+# jq_request <function name> <curl url and options>
+#
 jw_request () {
-  # $1: optional integer parameter for number of next-pages to retrieve
-  # $1 or $2 to $#: arguments to provide to curl
+  echodebug "jw_request args: $@"
 
-  echodebug "tfh_api_call args: $@"
-
-	if ! [ "$1" -eq "$1" ] >/dev/null 2>&1; then
-    npages="10000"
-  else
+  # Was a page limit provided?
+  if [ "$1" -eq "$1" ] >/dev/null 2>&1; then
+    echodebug "page limit is $1"
     npages="$1"
     shift
-	fi
 
-  echodebug "npages: $npages"
+    # If 0 was supplied get all the pages using a very large number
+    if [ $npages = 0 ]; then
+      echodebug "getting all pages due to page limit 0"
+      npages=100000
+    fi
+  fi
+
+  # Was a selector provided?
+  if [ -z "${1##.*}" ]; then
+    echodebug "selector provided for paging: $1"
+    selector="$1"
+    shift
+
+    # Get all the pages using a very large number
+    if [ -z "$npages" ]; then
+      echodebug "selector provided but no page limit, so getting all pages"
+      npages=100000
+    fi
+  fi
+
+  # Was a callback supplied?
+  echodebug "checking to see if "$1" is a callback"
+  if [ junonia_require_cmds "$1" 2>/dev/null ]; then
+    echodebug "found callback command"
+    callback="$1"
+    shift
+  fi
+
+  # No page limit, no selector. Get one page.
+  if [ -z "$npages" ] && [ -z "$selector" ]; then
+    echodebug "options specify getting a single page"
+    npages=1
+  fi
+
+  echodebug "npages:   $npages"
+  echodebug "selector: $selector"
+  echodebug "callback: $callback"
   echodebug "curl args: $@"
  
   if [ "$npages" -lt 1 ]; then
     return 0
   fi
 
-  case $curl_token_src in
+  case $JW_CURL_AUTH_SRC in
     curlrc)
-      echovvv "curl --header \"Content-Type: application/vnd.api+json\"" >&2
-      echovvv "     --config \"$curlrc\"" >&2
+      echovvv "curl --header \"Content-Type: $JW_CONTENT_TYPE\"" >&2
+      echovvv "     --config \"$JW_CURLRC\"" >&2
       echovvv "     $*" >&2
 
       resp="$(curl $curl_silent -w '\nhttp_code: %{http_code}\n' \
-                   --header "Content-Type: application/vnd.api+json" \
+                   --header "Content-Type: $JW_CONTENT_TYPE" \
                    --config "$curlrc" \
                    $@)"
       ;;
-    token)
-      echovvv "curl --header \"Content-Type: application/vnd.api+json\"" >&2
-      echovvv "     --header \"Authorization: Bearer \$TFH_token\"" >&2
+    oauth)
+      echovvv "curl --header \"Content-Type: $JW_CONTENT_TYPE\"" >&2
+      echovvv "     --header \"Authorization: Bearer \$JW_CURL_OAUTH\"" >&2
       echovvv "     $*" >&2
 
       resp="$(curl $curl_silent -w '\nhttp_code: %{http_code}\n' \
-                   --header "Content-Type: application/vnd.api+json" \
-                   --header "Authorization: Bearer $token" \
+                   --header "Content-Type: $JW_CONTENT_TYPE" \
+                   --header "Authorization: Bearer $JW_OAUTH" \
+                   $@)"
+      ;;
+    basic_auth)
+      echovvv "curl --header \"Content-Type: $JW_CONTENT_TYPE\"" >&2
+      echovvv "     --user \"$JW_CURL_BASIC\"" >&2
+      echovvv "     $*" >&2
+
+      resp="$(curl $curl_silent -w '\nhttp_code: %{http_code}\n' \
+                   --header "Content-Type: $JW_CONTENT_TYPE" \
+                   --user "$JW_CURL_BASIC" \
                    $@)"
       ;;
   esac
@@ -74,41 +162,32 @@ jw_request () {
 
   case "$resp_code" in
     2*)
+      # Output the response here
       printf "%s" "$resp_body"
 
-      next_page="$(printf "%s" "$resp_body" | \
-                   jq -r '.meta.pagination."next-page"' 2>&3)"
+      if [ -n $selector ]; then
+        next_page="$(printf "%s" "$resp_body" | \
+                     jq -r "$selector" 2>&3)"
+      elif [ -n $callback ]; then
+        next_page="$($callback "$resp_code" "$resp_body")"
+      else
+      fi
 
       if [ -n "$next_page" ] && [ "$next_page" != null ] &&
          ! [ "$npages" -le 1 ]; then
         echodebug "next page: $next_page"
         echodebug "npages: $npages"
-        next_link="$(printf "%s" "$resp_body" | jq -r '.links.next')"
+        next_link="$(printf "%s" "$resp_body" | jq -r "$selector")"
         echodebug "next link: $next_link"
-        tfh_api_call $((--npages)) "$next_link"
+        jw_request $((--npages)) "$selector" "$next_link"
       fi
       ;;
     4*|5*)
       echoerr "API request failed."
       echoerr_raw "HTTP status code: $resp_code"
-      if jsonapi_err="$(echo "$resp_body" | jq -r '
-        def leaf_print(o):
-          o.indent as $i |
-          $i + "  " as $ni |
-          o.errors as $e |
-          $e | keys[] as $k |
-            (select(($e[$k] | type) != "array" and ($e[$k] | type) != "object") |
-              "\($k): \($e[$k])"),
-            (select(($e[$k] | type) == "object") |
-              "\($k):",
-              "\(leaf_print({"errors": $e[$k], "indent": $ni}))"),
-            (select(($e[$k] | type) == "array") |
-              "\($k):",
-              "\(leaf_print({"errors": $e[$k], "indent": $ni}))");
-
-        leaf_print({"errors": .errors[], "indent": "  "})')"; then
-        echoerr_raw "JSON-API details:"
-        echoerr_raw "$jsonapi_err"
+      if json_err="$(jw_tree_print "$resp_body" "$JW_ERR_SELECTOR")"; then
+        echoerr_raw "Details:"
+        echoerr_raw "$json_err"
       else
         echoerr "Response:"
         echoerr_raw "$resp_body"
@@ -126,142 +205,74 @@ jw_request () {
   esac
 }
 
-check_required () {
-  if [ 0 -eq $# ]; then
-    check_for="org ws token address"
-  else
-    check_for="$*"
+_jw_filter () {
+  readonly JW_DEFAULT_CURLRC="$JUNONIA_CONFIGDIR/curlrc"
+
+  JW_CURLRC="${1:-"$JW_DEFAULT_CURLRC"}"
+  readonly JW_OAUTH="$2"
+  readonly JW_BASIC_AUTH="$3"
+
+  JW_CURL_AUTH_SRC=none
+
+  if [ -f "$curlrc" ]; then
+    JW_CURL_AUTH_SRC=curlrc
   fi
 
-  missing=0
-  for i in $check_for; do
-    case "$i" in
-      org)
-        if [ -z "$org" ]; then
-          missing=1
-          echoerr 'TFE organization required.'
-          echoerr 'Set with $TFH_org or use -org'
-          echoerr
-        fi
-      ;;
-      ws)
-        if [ -z "$ws" ]; then
-          missing=1
-          echoerr 'TFE workspace name required.'
-          echoerr 'Set with $TFH_name or use -name, and optionally -prefix'
-          echoerr
-        fi
-      ;;
-      token)
-        if [ "$curl_token_src" = none ]; then
-          missing=1
-          echoerr 'TFE API token required.'
-          echoerr 'Set with `tfh curl-config`,  $TFH_token, or -token'
-          echoerr
-        fi
-      ;;
-      address)
-        # This really shouldn't happen. Someone would have to
-        # explicitly pass in an empty string to the command line
-        # argument.
-        if [ -z "$address" ]; then
-          missing=1
-          echoerr 'TFE hostname required.'
-          echoerr 'Set with -hostname or $TFH_hostname'
-          echoerr
-        fi
-      ;;
-    esac
-  done
-  return $missing
-}
+  readonly JW_CURLRC
 
-tfh_junonia_filter () {
-  readonly TFH_DEFAULT_CURLRC="$JUNONIA_CONFIGDIR/curlrc"
+  echodebug "JW_CURLRC:     $JW_CURLRC"
+  echodebug "JW_OAUTH:      $JW_OAUTH"
+  echodebug "JW_BASIC_AUTH: $JW_BASIC_AUTH"
 
-  readonly org="$1"
-  readonly name="$2"
-  readonly prefix="$3"
-  readonly token="$4"
-  readonly curlrc="${5:-"$TFH_DEFAULT_CURLRC"}"
-  readonly hostname="$6"
-
-  # Waterfall verbosity levels down
-  readonly vvverbose="$9"
-  readonly vverbose="${8:-$vvverbose}"
-  readonly verbose="${7:-$vverbose}"
-
-  readonly address="https://$hostname"
-  readonly ws="$prefix$name"
-
-  echov "org:       $org"
-  echov "prefix:    $prefix"
-  echov "workspace: $name"
-  echov "hostname:  $hostname"
-  echov "address:   $address"
-  echov "verbose:   $verbose"
-  echov "vverbose:  $vverbose"
-  echov "vvverbose: $vvverbose"
-
-  curl_token_src=
-
-  # curlrc argument at the command line takes highest precedence
-  if echo "$TFH_CMDLINE" | grep -qE -- '-curlrc'; then
-    echodebug "explicit -curlrc"
-    if [ -f "$curlrc" ]; then
-      curl_token_src=curlrc
-    else
-      curl_token_src=curlrc_not_found
-    fi
+  # OAuth at the command line takes second highest precedence
+  if [ -z "$JW_CURL_AUTH_SRC" ] && [ -n "$JW_OAUTH" ] &&
+     echo "$JUNONIA_ARGS" | grep -qE -- ' -oauth '; then
+     echodebug "explicit -oauth"
+     JW_CURL_AUTH_SRC=oauth
   fi
 
-  # token at the command line takes second highest precedence
-  if [ -z "$curl_token_src" ] && [ -n "$token" ] &&
-     echo "$TFH_CMDLINE" | grep -qE -- '-token'; then
-    echodebug "explicit -token"
-    curl_token_src=token
+  # Basic auth at the command line takes third highest precedence
+  if [ -z "$JW_CURL_AUTH_SRC" ] && [ -n "$JW_BASIC_AUTH" ] &&
+     echo "$JUNONIA_ARGS" | grep -qE -- ' -basic-auth '; then
+     echodebug "explicit -basic-auth"
+     JW_CURL_AUTH_SRC=basic
   fi
 
-  # curlrc from any source (default included) comes third
-  if [ -z "$curl_token_src" ] && [ -f "$curlrc" ]; then
+  # curlrc from any source (default included) comes fourth
+  if [ -z "$JW_CURL_AUTH_SRC" ] && [ -f "$JW_CURLRC" ]; then
     echodebug "curlrc from env and config file"
-    curl_token_src=curlrc
+    JW_CURL_AUTH_SRC=curlrc
   fi
 
-  # token from the config file or environment var comes last
-  if [ -z "$curl_token_src" ] && [ -n "$token" ]; then
-    echodebug "token from env and config file"
-    curl_token_src=token
+  if [ -z "$JW_CURL_AUTH_SRC" ]; then
   fi
 
-  if [ -z "$curl_token_src" ]; then
-    curl_token_src=none
-  fi
-
-  if [ -z "$token" ]; then
-    token_status="empty"
-  else
-    token_status="not empty"
-  fi
-
-  case $curl_token_src in
+  case $JW_CURL_AUTH_SRC in
     curlrc)
-      echov "token:     $token_status, unused"
-      echov "curlrc:    $curlrc"
+      echodebug "basic:     $token_status, unused"
+      echodebug "curlrc:    $curlrc"
       ;;
-    token)
-      echov "token:     $token_status"
-      echov "curlrc:    $curlrc, unused"
+    oauth)
+      echodebug "token:     $token_status"
+      echodebug "curlrc:    $curlrc, unused"
+      ;;
+    basic)
+      echodebug "token:     $token_status"
+      echodebug "curlrc:    $curlrc, unused"
       ;;
     curlrc_not_found)
-      echov "token:     $token_status, unused"
-      echov "curlrc:    $curlrc specified but not found"
+      echodebug "token:     $token_status, unused"
+      echodebug "curlrc:    $curlrc specified but not found"
       ;;
     none)
-      echov "token:     empty"
-      echov "curlrc:    $curlrc not found"
+      echodebug "token:     empty"
+      echodebug "curlrc:    $curlrc not found"
       ;;
   esac
+
+  readonly JW_CURLRC="$1"
+  readonly JW_OAUTH="$2"
+  readonly JW_BASIC_AUTH="$3"
 
   return 9
 }
